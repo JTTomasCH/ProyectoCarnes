8d9933e1f69d70082c4f8d7981d7455b36112ddd
tomaschirivi <tomaschirivi@gmail.com>
2024-11-25 13:03:26 -0500

    Cambio en iniciarSesion.php

--- iniciarSesion.php
+++ iniciarSesion.php
@@ -41,6 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         error_log("Valor de activo: " . $activo);
 
         if ($activo == 1 && password_verify($contrasena, $hashed_password)) {
+            $_SESSION['id'] = $id;
             $_SESSION['nombres'] = $nombres;
             $_SESSION['apellidos'] = $apellidos;
             $_SESSION['correo'] = $correo;
@@ -65,7 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 header("Location: ../index2.php");
             }
 
-            if ($correo === 'byrcarnes@gmail.com' && $id_cargo == 1) {
+            if ($correo === 'byrcarnes@gmail.com' && $id_cargo == 1 && $id == 72) {
                 error_log("Redirigiendo a admin.php");
                 header("Location: ../grafica.php");
             } else {
